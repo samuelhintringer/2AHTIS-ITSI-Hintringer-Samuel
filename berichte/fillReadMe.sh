0df1d9aed4cacc88293156acd7a15bc074b421b2
samuelhintringer <samuel.hintringer@htl-braunau.at>
2026-06-17 07:09:57 +0000

    commit

---
+++ berichte/fillReadMe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+{
+    echo -e "# Arbeitsberichte"\n
+    for file in 2*.md; do
+        basename=$(basename "$file" .md)
+
+        jahr="20${basename:0:2}"
+        monat="${basename:2:2}"
+        tag="${basename:4:2}"
+
+        echo "[Arbeitsbericht vom ${tag}.${monat}.${jahr}](berichte/${basename}.html)"
+    done
+} > ReadMeLinks.tx > README.md
