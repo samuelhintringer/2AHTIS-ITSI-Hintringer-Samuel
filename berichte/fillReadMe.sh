4bb381c2fa4430724cc15d3eed55775274f65448
samuelhintringer <samuel.hintringer@htl-braunau.at>
2026-06-17 07:17:58 +0000

    update

--- berichte/fillReadMe.sh
+++ berichte/fillReadMe.sh
@@ -10,4 +10,17 @@
 
         echo "[Arbeitsbericht vom ${tag}.${monat}.${jahr}](berichte/${basename}.html)"
     done
-} > ReadMeLinks.tx > README.md
+} > ReadMeLinks.tx
+
+{
+    echo -e "# Arbeitsberichte"\n
+    for file in 2*.md; do
+        basename=$(basename "$file" .md)
+
+        jahr="20${basename:0:2}"
+        monat="${basename:2:2}"
+        tag="${basename:4:2}"
+
+        echo "[Arbeitsbericht vom ${tag}.${monat}.${jahr}](berichte/${basename}.html)"
+    done
+} > ../README.md
