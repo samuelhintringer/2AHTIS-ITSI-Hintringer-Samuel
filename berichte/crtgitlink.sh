6ba5cbf62c17a07ac3e0288855f99c9ad13ec4a2
samuelhintringer <samuel.hintringer@htl-braunau.at>
2026-06-23 20:42:25 +0000

    update

--- berichte/crtgitlink.sh
+++ berichte/crtgitlink.sh
@@ -3,8 +3,9 @@
 reponame="$(git config --get remote.origin.url | rev | cut -d/ -f1 | rev | cut -d. -f1)"
 link="https://samuelhintringer.github.io/$reponame/berichte/"
 
+{
 for b in $(ls 2*.md)
 do
 	echo -e $link${b/md/html}"\n"
 done
-
+} > gitlinks.md
